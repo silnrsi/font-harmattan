64d8a236e4d442974fec16e27dce3e44369c4d73
Bob Hallissy <bobh0303@users.noreply.github.com>
2019-10-13 15:35:44 -0600

    Add graide files

---
+++ graide/graide.bat
@@ -0,0 +1,11 @@
+set ver=0_8_80
+
+@echo off
+
+@rem get source file if not present
+if not exist ..\results\generated\Harmattan-Regular.xml (
+	echo "file ..\results\generated\Harmatta-Regular.xml is missing ... please build font first."
+) else (
+	if not exist ..\results\Harmattan-Regular-graide.ttf psfufo2ttf ..\source\Harmattan-Regular.ufo ..\results\Harmattan-Regular-graide.ttf
+	graide%ver%_console.exe -p Harmattan.cfg 
+)
