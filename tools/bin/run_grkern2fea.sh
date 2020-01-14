d5f39335903b655a1a114bc73f39840fdb334fbf
Bob Hallissy <bobh0303@users.noreply.github.com>
2020-01-14 16:36:09 -0700

    Split bold and regular kerning values

--- tools/bin/run_grkern2fea.sh
+++ tools/bin/run_grkern2fea.sh
@@ -1,32 +1,40 @@
 #!/bin/sh
 
 # Assumes we're in a folder one level below root, e.g., font-harmattan/dev
+# Puts results into ../source/opentype
 
-# Parse ftml and create raw data list
-echo reading ../tests/kerndata.ftml...
-../tools/bin/grstrings.py -i ../tests/kerndata.ftml -t -f ../results/Harmattan-Regular.ttf -r rawPairData-ftml.txt
+set +x
+
+# Puts results in subfolder
+outdir=grkern2fea_res
+mkdir -p $outdir
+
+for face in Regular Bold
+do
+	echo "\n*****" Parse ../tests/kerndata.ftml and generate raw kern data for $face...
+	../tools/bin/grstrings.py -i ../tests/kerndata.ftml -t -f ../results/Harmattan-$face.ttf -r $outdir/rawPairData-$face.txt
+	echo "\n*****" Generate fea output for $face
+	../tools/bin/glyphstring.py -f ../results/Harmattan-$face.ttf  -r 20 -j 1    -R $outdir/rawPairData-$face.txt $outdir/caKern-$face.fea
+	echo "\n*****" Update mark references and copy fea to source tree...
+	sed -e s/kasratan-ar/@_diaB/g -e s/fathatan-ar/@_diaA/g $outdir/caKern-$face.fea  > ../source/opentype/caKern-$face.fea
+done
+echo "\nDone.\n"
+exit
+
 
 # Trace graphite to get kerning data
 
-# Phase 0:
-# ../tools/bin/glyphstring.py -f ../results/Harmattan-Regular.ttf  -r 20 -j 1 -e 0    rawPairData-ftml.txt results/kernStrings-e0.txt
 
-# Phase 1:
-# ../tools/bin/glyphstring.py -f ../results/Harmattan-Regular.ttf  -r 20 -j 1 -e 1    rawPairData-ftml.txt results/kernStrings-e1.txt
+# generate fea
+echo generating Regular fea output...
 
-# Phase 2:
-# echo generating -e2 output...
-# ../tools/bin/glyphstring.py -f ../results/Harmattan-Regular.ttf  -r 20 -j 1 -e 2    rawPairData-ftml.txt results/kernStrings-e2.txt
 
-# Phase 3: generate fea
-echo generating fea output...
-../tools/bin/glyphstring.py -f ../results/Harmattan-Regular.ttf  -r 20 -j 1      -R rawPairData-ftml.txt caKern.fea
+echo generating Bold fea output...
+../tools/bin/glyphstring.py -f ../results/Harmattan-Bold.ttf     -r 20 -j 1      -R rawPairData-Bold.txt    res/caKern-bold.fea
 
-# Copy result to source folder so project will compile with shortened version:
+
+# Copy result to source folder so project will compile:
 echo updating mark references and copying results to source tree...
-sed -e s/kasratan-ar/@_diaB/g -e s/fathatan-ar/@_diaA/g caKern.fea > ../source/opentype/caKern.fea
+sed -e s/kasratan-ar/@_diaB/g -e s/fathatan-ar/@_diaA/g res/caKern.fea > ../source/opentype/caKern.fea
 
-
-# check the results (only works if RHS is one glyph)
-# echo Number of potential problems:
-# ../tools/bin/splitResults.py kernStrings-e2.txt kernStrings-flattened-check.txt | wc -l
+echo ../source/opentype/caKern.fea updated
