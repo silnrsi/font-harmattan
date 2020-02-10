c01c97b4b06f210432638eaa8745ea5e3e8d3384
Bob Hallissy <bobh0303@users.noreply.github.com>
2020-02-10 13:28:02 -0600

    replace grstrings & glyphstring with grkern2fea

--- tools/bin/run_grkern2fea.sh
+++ tools/bin/run_grkern2fea.sh
@@ -3,19 +3,21 @@
 # Assumes we're in a folder one level below root, e.g., font-harmattan/dev
 # Puts results into ../source/opentype
 
-set +x
+# R=20
+
+set -x
+set -e
 
 # Puts results in subfolder
-outdir=grkern2fea_res
+outdir=grkern2fea_res7_r${R:=20}
 mkdir -p $outdir
 
-for face in Regular Bold
-do
-	echo "\n*****" Parse ../tests/kerndata.ftml and generate raw kern data for $face...
-	../tools/bin/grstrings.py -i ../tests/kerndata.ftml -t -f ../results/Harmattan-$face.ttf -r $outdir/rawPairData-$face.txt
-	echo "\n*****" Generate fea output for $face
-	../tools/bin/glyphstring.py -f ../results/Harmattan-$face.ttf  -r 20 -j 1    -R $outdir/rawPairData-$face.txt $outdir/caKern-$face.fea
-	echo "\n*****" Update mark references and copy fea to source tree...
-	sed -e s/kasratan-ar/@_diaB/g -e s/fathatan-ar/@_diaA/g $outdir/caKern-$face.fea  > ../source/opentype/caKern-$face.fea
-done
-echo "\nDone.\n"
+grkern2fea -e graphite -i ../tests/kerndata.ftml          -f ../results/Harmattan-Regular.ttf           $outdir/rawPairData-Regular.txt ; \
+grkern2fea -s strings  -i $outdir/rawPairData-Regular.txt -f ../results/Harmattan-Regular.ttf  -r ${R:=20} -R $outdir/caKern-Regular.fea; \
+sed -e s/kasratan-ar/@_diaB/g -e s/fathatan-ar/@_diaA/g $outdir/caKern-Regular.fea  > ../source/opentype/caKern-Regular.fea &
+
+grkern2fea -e graphite -i ../tests/kerndata.ftml         -f ../results/Harmattan-Bold.ttf           $outdir/rawPairData-Bold.txt ; \
+grkern2fea -s strings -i $outdir/rawPairData-Bold.txt    -f ../results/Harmattan-Bold.ttf  -r ${R:=20} -R $outdir/caKern-Bold.fea; \
+sed -e s/kasratan-ar/@_diaB/g -e s/fathatan-ar/@_diaA/g $outdir/caKern-Bold.fea  > ../source/opentype/caKern-Bold.fea &
+
+wait
