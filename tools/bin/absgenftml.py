d76637256fc49cd2731c19301a8a120e260aec15
Bob Hallissy <bobh0303@users.noreply.github.com>
2020-04-24 11:07:30 -0600

    test cv82 inside subtending mark

--- tools/bin/absgenftml.py
+++ tools/bin/absgenftml.py
@@ -598,7 +598,7 @@ def doit(args):
             ('cv76', {'sd': 1, 'ur': 1, 'ku': 1, 'rhg': 1, 'wo': 1}, (0x0633, 0x0670), 'Superscript alef alternates'),
             ('cv78', {'sd': 1, 'ur': 1, 'ku': 1, 'rhg': 1, 'wo': 1}, (0x0652,), 'Sukun alternates'),
             ('cv80', {'sd': 1, 'ur': 1, 'ku': 1, 'rhg': 1, 'wo': 1}, (0x06DD,), 'Ayah alternates'),
-            ('cv82', {'sd': 1, 'ur': 1, 'ku': 1, 'rhg': 1, 'wo': 1}, (0x06F4, 0x06F6, 0x06F7), 'Eastern Digit alternates'),
+            ('cv82', {'sd': 1, 'ur': 1, 'ku': 1, 'rhg': 1, 'wo': 1}, (0x06F4, 0x06F6, 0x06F7, 0x0020, 0x06DD, 0x06F4, 0x06F6, 0x06F7), 'Eastern Digit alternates'),
             ('cv84', {'sd': 1, 'ur': 1, 'ku': 1, 'rhg': 1, 'wo': 1}, (0x060C,), 'Comma alternates'),
         )
 
