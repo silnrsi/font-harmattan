194122bb2f1d3e4fc6fc805410a18992588dbc7a
BobH <bob_hallissy@sil.org>
2023-01-25 16:20:39 -0700

    merge features from Lateef's version to updateKerning.sh, commit updated octaboxes

--- tools/updateKerning.sh
+++ tools/updateKerning.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script rebuilds the kerning files for both Graphite and OpenType. See README.md
 
@@ -10,7 +10,12 @@
 # $R is the clustering radius for computing the OpenType kerning. Default is 20 but can be overridden, e.g.:
 #      export R=50 updateKerning
 
-# --nooctalap causes script to assume optimized octaboxes needn't be recomputed
+# Command line options:
+#    --ftml        rebuild ftml before rebuilding kerning
+#    --ftmlonly    rebuild only ftml
+#    --octalap     rebuild optimized octaboxes (otherwise assumes they are acceptable)
+#    --regOnly     process only the Regular font rather than Regular and Bold
+#    --noKern      debugging tool to skip the actual building of fea files 
 
 set -e	# Stop on error
 # set -x	# echo before execution
@@ -21,61 +26,138 @@ then
 	exit 2
 fi
 
-echo "Updating kerning\nrebuilding fonts without glyph kerning or renaming...\n"
+# Default settings:
+REGONLY=""
+WEIGHTS=(Regular Bold)
+OCTALAP=0
+FTML=0
+FTMLONLY=0
+KERN=1
+
+# Look for options:
+while [[ $# -gt 0 ]]
+do
+  case $1 in
+    --regOnly)
+    REGONLY="--regOnly"
+    WEIGHTS=(Regular)
+    ;;
+  
+    --octalap)
+    OCTALAP=1
+    ;;
+
+    --ftml)
+    FTML=1
+    ;;
+    
+    --ftmlonly)
+    FTML=2
+    ;;
+
+    --noKern)
+    KERN=0
+    ;;
+
+    *)
+    echo "unrecognized parameter $1"
+    echo "Command line options:"
+    echo "   --ftml         rebuild ftml"
+    echo "   --ftmlonly     rebuild ftml but do nothing else"
+    echo "   --octalap      rebuild optimized octaboxes"
+    echo "   --regOnly      process only the Lateef Regular font rather than all six"
+    exit 
+  esac
+  shift
+done
+
+if [ ${FTML} -gt 0 ]
+then
+  echo "rebuilding kerndata.ftml..."
+
+  tools/absgenftml.py -q -t "KernData with Marks (auto)"  --norendercheck  --ap "_?dia[AB]$" \
+        -l source/logs/kerndata.log \
+        --xsl ../tools/lib/ftml.xsl --scale 250 -i source/glyph_data.csv -w 75% \
+        -s "url(../results/Harmattan-Regular.ttf)|Reg-Gr" \
+        -s "url(../results/tests/ftml/fonts/Harmattan-Regular_ot_arab.ttf)|Reg-OT" \
+        -s "url(../results/Harmattan-Bold.ttf)|Bld-Gr" \
+        -s "url(../results/tests/ftml/fonts/Harmattan-Bold_ot_arab.ttf)|Bld-OT" \
+        source/Harmattan-Regular.ufo source/kerndata.ftml 
+fi
+
+if [ ${FTML} -gt 1 ]
+then
+  echo "finished successfullly and source/kerndata.ftml has been rebuilt."
+  exit
+fi
+
+echo -e "\nUpdating kerning\n"
+
+echo -e "\nrebuilding with graphite and fonts without glyph kerning or renaming...\n"
 
 smith distclean
-smith configure
-smith build --quick --norename
+smith configure --graphite
+smith build --graphite --quick --norename $REGONLY
 
-if [ "z$1" != "z--nooctalap" ]; then
-echo "\nrebuilding optimized octaboxes...\n"
-
-octalap -q -j 0 -o source/Harmattan-Regular-octabox.json results/Harmattan-Regular.ttf  &
-octalap -q -j 0 -o source/Harmattan-Bold-octabox.json    results/Harmattan-Bold.ttf 
-
-wait
-
-echo "\nrebuilding fonts (with new octaboxes)...\n" 
-
-smith clean ;
-smith build --quick --norename 
+if [ ${OCTALAP} == 1 ]; 
+then
+  echo -e "\nrebuilding optimized octaboxes...\n"
+  
+  for w in "${WEIGHTS[@]}"
+  do
+    octalap -q -j 0 -o source/Harmattan-$w-octabox.json results/Harmattan-$w.ttf  &
+  done
+  wait
+  
+  echo -e "\nrebuilding fonts with graphite and new octaboxes but without glyph kerning or renaming...\n" 
+  
+  smith clean ;
+  smith build --graphite --quick --norename $REGONLY
 fi
 
-echo "\nrebuilding source/kerndata.ftml\n"
+if [ ${KERN} == 1 ]
+then
+  echo -e "\nrebuilding collision-avoidance-based kerning...\n"
 
-tools/absgenftml.py -q -t "KernData with Marks (auto)" source/Harmattan-Regular.ufo source/kerndata.ftml  -l source/logs/kerndata.log --norendercheck  --ap "_?dia[AB]$" --xsl ../tools/lib/ftml.xsl --scale 250 -i source/glyph_data.csv -w 75% -s "url(../results/Harmattan-Regular.ttf)|Reg-Gr" -s "url(../results/tests/ftml/fonts/Harmattan-Regular_ot_arab.ttf)|Reg-OT" -s "url(../results/Harmattan-Bold.ttf)|Bld-Gr" -s "url(../results/tests/ftml/fonts/Harmattan-Bold_ot_arab.ttf)|Bld-OT" 
+  # Use a temp directory
+  outdir=results/grkern2fea_r${R:=20}
+  mkdir -p $outdir
 
-echo "\nrebuilding collision-avoidance-based kerning...\n"
-
-# Use a temp directory
-outdir=results/grkern2fea_r${R:=20}
-mkdir -p $outdir
-
-( \
-  grkern2fea -e graphite -i source/kerndata.ftml -F ut53=0        -f results/Harmattan-Regular.ttf                 $outdir/rawPairData-Regular.txt        ; \
-  tools/renumberKernData.py $outdir/rawPairData-Regular.txt                                                        $outdir/rawPairData-Regular-nozwj.txt  ; \
-  grkern2fea -s strings  -i $outdir/rawPairData-Regular-nozwj.txt -f results/Harmattan-Regular.ttf  -r ${R:=20} -R $outdir/caKern-Regular.fea             ; \
-  sed -e s/kasratan-ar/@_diaB/g -e s/fathatan-ar/@_diaA/g $outdir/caKern-Regular.fea  > source/opentype/caKern-Regular.fea \
-) &
-
-( \
-  grkern2fea -e graphite -i source/kerndata.ftml -F ut53=0        -f results/Harmattan-Bold.ttf                    $outdir/rawPairData-Bold.txt           ; \
-  tools/renumberKernData.py $outdir/rawPairData-Bold.txt                                                           $outdir/rawPairData-Bold-nozwj.txt     ; \
-  grkern2fea -s strings  -i $outdir/rawPairData-Bold-nozwj.txt    -f results/Harmattan-Bold.ttf     -r ${R:=20} -R $outdir/caKern-Bold.fea                ; \
-  sed -e s/kasratan-ar/@_diaB/g -e s/fathatan-ar/@_diaA/g $outdir/caKern-Bold.fea  > source/opentype/caKern-Bold.fea \
-) &
-
-wait
+  for w in "${WEIGHTS[@]}"
+  do
+    ( \
+      grkern2fea -e graphite -i source/kerndata.ftml -F ut53=0        -f results/Harmattan-$w.ttf                 $outdir/rawPairData-$w.txt        ; \
+      tools/renumberKernData.py $outdir/rawPairData-$w.txt                                                        $outdir/rawPairData-$w-nozwj.txt  ; \
+      grkern2fea -s strings  -i $outdir/rawPairData-$w-nozwj.txt -f results/Harmattan-$w.ttf  -r ${R:=20} -R $outdir/caKern-$w.fea             ; \
+      sed -e s/kasratan-ar/@_diaB/g -e s/fathatan-ar/@_diaA/g $outdir/caKern-$w.fea  > source/opentype/caKern-$w.fea \
+    ) &
+  done
+  wait
+fi
 
 echo "finished successfullly, and the following files were regenerated:"
-if [ "z$1" != "z--nooctalap" ]; then
-echo "  - source/Harmattan-Regular-octabox.json
-  - source/Harmattan-Bold-octabox.json"
+if [ ${FTML} -gt 0]
+then
+  echo " - source/kerndata.ftml"
 fi
 
-echo "  - source/opentype/caKern-Regular.fea
-  - soure/opentype/caKern-Bold.fea
+if [ ${OCTALAP} == 1 ] 
+then
+  for w in "${WEIGHTS[@]}" 
+  do
+      echo " - source/Harmattan-$w-octabox.json"
+  done
+fi
 
+if [ ${KERN} ]
+then
+  for w in "${WEIGHTS[@]}"
+  do
+    echo " - source/opentype/caKern-$w.fea"
+  done
+fi
+
+echo "
 Notes:
   - Intermediate files are in $outdir
   - The fonts have not been rebuilt with these modified files. To complete the build, use:
