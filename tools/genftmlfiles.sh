f350fa6f49c75a8d3b9c9dcb2d03190d5c579cf7
BobH <bob_hallissy@sil.org>
2024-03-27 13:44:45 -0600

    genftmlfiles.sh: Organize common parameters into an array; rebuild ftml for previous changes [nobuild]

--- tools/genftmlfiles.sh
+++ tools/genftmlfiles.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script rebuilds the algorithmically-generated ftml files. See README.md
 
@@ -18,16 +18,30 @@ fi
 prevfont="references/v4.000/Harmattan-Regular.ttf"
 prevver="4.0"
 
+commonParams=( \
+	--prevfont "$prevfont"  \
+	-s "url(../$prevfont)|$prevver"  \
+	--ap '_?dia[AB]$'  \
+	--xsl ../tools/ftml.xsl  \
+	--scale 250  \
+	-i source/glyph_data.csv  \
+	--langs 'sd,ur,ku,rhg,ky,wo'  \
+	-w 75%  \
+	--ucdxml source/additional_ucd.xml  \
+	-s "url(../references/Harmattan-Regular.ttf)|ref"  \
+	-s "url(../results/Harmattan-Regular.ttf)|Reg"  \
+)
+
 echo "Rebuilding ftml files..."
-tools/absgenftml.py -q -t "AllChars (auto)"                      source/masters/Harmattan-Regular.ufo  tests/AllChars-auto.ftml        -l logs/AllChars.log        --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 250 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s "url(../references/Harmattan-Regular.ttf)|ref" -s "url(../results/Harmattan-Regular.ttf)|Reg" -s "url(../results/Harmattan-Medium.ttf)|Med" -s "url(../results/Harmattan-SemiBold.ttf)|SeBld" -s "url(../results/Harmattan-Bold.ttf)|Bold" &
-tools/absgenftml.py -q -t "AL Sorted (auto)"                     source/masters/Harmattan-Regular.ufo  tests/ALsorted-auto.ftml        -l logs/ALsorted.log        --prevfont "$prevfont"                                 --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 250 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s "url(../references/Harmattan-Regular.ttf)|ref" -s "url(../results/Harmattan-Regular.ttf)|Reg" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
-tools/absgenftml.py -q -t "DiacTest1 (auto)"                     source/masters/Harmattan-Regular.ufo  tests/DiacTest1-auto.ftml       -l logs/DiacTest1.log       --prevfont "$prevfont"                                 --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 250 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s "url(../references/Harmattan-Regular.ttf)|ref" -s "url(../results/Harmattan-Regular.ttf)|Reg" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
-tools/absgenftml.py -q -t "DiacTest1 Short (auto)"               source/masters/Harmattan-Regular.ufo  tests/DiacTest1-short-auto.ftml -l logs/DiacTest1-short.log --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 250 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s "url(../references/Harmattan-Regular.ttf)|ref" -s "url(../results/Harmattan-Regular.ttf)|Reg" -s "url(../results/Harmattan-Medium.ttf)|Med" -s "url(../results/Harmattan-SemiBold.ttf)|SeBld" -s "url(../results/Harmattan-Bold.ttf)|Bold" &
-tools/absgenftml.py -q -t "Subtending Marks (auto)"              source/masters/Harmattan-Regular.ufo  tests/SubtendingMarks-auto.ftml -l logs/Subtending.log      --prevfont "$prevfont"                                 --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 250 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s "url(../references/Harmattan-Regular.ttf)|ref" -s "url(../results/Harmattan-Regular.ttf)|Reg" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
-tools/absgenftml.py -q -t "DaggerAlef (auto)"                    source/masters/Harmattan-Regular.ufo  tests/DaggerAlef-auto.ftml      -l logs/DaggerAlef.log      --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 250 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s "url(../references/Harmattan-Regular.ttf)|ref" -s "url(../results/Harmattan-Regular.ttf)|Reg" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
-tools/absgenftml.py -q -t "Yehbarree (auto)"                     source/masters/Harmattan-Regular.ufo  tests/Yehbarree-auto.ftml       -l logs/Yehbarree.log       --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 250 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s "url(../references/Harmattan-Regular.ttf)|ref" -s "url(../results/Harmattan-Regular.ttf)|Reg" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
-tools/absgenftml.py -q -t "Feature-Language Interactions (auto)" source/masters/Harmattan-Regular.ufo  tests/FeatLang-auto.ftml        -l tests/logs/FeatLang.log  --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 250 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s "url(../references/Harmattan-Regular.ttf)|ref" -s "url(../results/Harmattan-Regular.ttf)|Reg" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
-tools/absgenftml.py -q -t "Kerning (auto)"                       source/masters/Harmattan-Regular.ufo  tests/Kern-auto.ftml            -l logs/Kerning.log         --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 250 -i source/glyph_data.csv --langs "sd,ur,ku,rhg,ky,wo" -w 75% --ucdxml source/additional_ucd.xml -s "url(../references/Harmattan-Regular.ttf)|ref" -s "url(../results/Harmattan-Regular.ttf)|Reg" -s "url(../results/Harmattan-Medium.ttf)|Med" -s "url(../results/Harmattan-SemiBold.ttf)|SeBld" -s "url(../results/Harmattan-Bold.ttf)|Bold" &
-# tools/absgenftml.py -q -t "KernData with Marks (auto)"         source/masters/Harmattan-Regular.ufo  source/kerndata.ftml            -l source/logs/kerndata.log --prevfont "$prevfont" -s "url(../$prevfont)|$prevver" --ap "_?dia[AB]$" --xsl ../tools/ftml.xsl --scale 250 -i source/glyph_data.csv    --norendercheck    -f h   -w 75% --ucdxml source/additional_ucd.xml -s "url(../references/Harmattan-Regular.ttf)|ref" -s "url(../results/Harmattan-Regular.ttf)|Reg" -s "url(../results/Harmattan-Medium.ttf)|Med" -s "url(../results/Harmattan-SemiBold.ttf)|SeBld" -s "url(../results/Harmattan-Bold.ttf)|Bold" &
+tools/absgenftml.py -q -t "AllChars (auto)"                      source/masters/Harmattan-Regular.ufo  tests/AllChars-auto.ftml        -l logs/AllChars.log        "${commonParams[@]}" -s "url(../results/Harmattan-Medium.ttf)|Med" -s "url(../results/Harmattan-SemiBold.ttf)|SeBld" -s "url(../results/Harmattan-Bold.ttf)|Bold" &
+tools/absgenftml.py -q -t "AL Sorted (auto)"                     source/masters/Harmattan-Regular.ufo  tests/ALsorted-auto.ftml        -l logs/ALsorted.log        "${commonParams[@]}" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
+tools/absgenftml.py -q -t "DiacTest1 (auto)"                     source/masters/Harmattan-Regular.ufo  tests/DiacTest1-auto.ftml       -l logs/DiacTest1.log       "${commonParams[@]}" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
+tools/absgenftml.py -q -t "DiacTest1 Short (auto)"               source/masters/Harmattan-Regular.ufo  tests/DiacTest1-short-auto.ftml -l logs/DiacTest1-short.log "${commonParams[@]}" -s "url(../results/Harmattan-Medium.ttf)|Med" -s "url(../results/Harmattan-SemiBold.ttf)|SeBld" -s "url(../results/Harmattan-Bold.ttf)|Bold" &
+tools/absgenftml.py -q -t "Subtending Marks (auto)"              source/masters/Harmattan-Regular.ufo  tests/SubtendingMarks-auto.ftml -l logs/Subtending.log      "${commonParams[@]}" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
+tools/absgenftml.py -q -t "DaggerAlef (auto)"                    source/masters/Harmattan-Regular.ufo  tests/DaggerAlef-auto.ftml      -l logs/DaggerAlef.log      "${commonParams[@]}" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
+tools/absgenftml.py -q -t "Yehbarree (auto)"                     source/masters/Harmattan-Regular.ufo  tests/Yehbarree-auto.ftml       -l logs/Yehbarree.log       "${commonParams[@]}" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
+tools/absgenftml.py -q -t "Feature-Language Interactions (auto)" source/masters/Harmattan-Regular.ufo  tests/FeatLang-auto.ftml        -l tests/logs/FeatLang.log  "${commonParams[@]}" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
+tools/absgenftml.py -q -t "Kerning (auto)"                       source/masters/Harmattan-Regular.ufo  tests/Kern-auto.ftml            -l logs/Kerning.log         "${commonParams[@]}" -s "url(../results/Harmattan-Medium.ttf)|Med" -s "url(../results/Harmattan-SemiBold.ttf)|SeBld" -s "url(../results/Harmattan-Bold.ttf)|Bold" &
+# tools/absgenftml.py -q -t "KernData with Marks (auto)"         source/masters/Harmattan-Regular.ufo  source/kerndata.ftml            -l source/logs/kerndata.log "${commonParams[@]}" -s "url(../results/Harmattan-Medium.ttf)|Med" -s "url(../results/Harmattan-SemiBold.ttf)|SeBld" -s "url(../results/Harmattan-Bold.ttf)|Bold" &
 wait
 echo done.
