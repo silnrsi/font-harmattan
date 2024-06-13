8492b3d2b85a7e21eb6589902d6dd8feb4887086
BobH <bob_hallissy@sil.org>
2024-06-13 17:27:47 -0600

    [ABS-3071, ABS-3053] Support overriding language behavior and Kashmirir langauge (incl TypeTuner); misc tidying of shared code.

--- tools/genftmlfiles.sh
+++ tools/genftmlfiles.sh
@@ -32,6 +32,9 @@ commonParams=( \
 	-s "url(../results/Harmattan-Regular.ttf)|Reg"  \
 )
 
+#tools/absgenftml.py -q -t "Language-Direction Interactions (auto)" source/masters/Harmattan-Regular.ufo  tests/Lang-Dir-auto.ftml        -l tests/logs/LangDir.log  "${commonParams[@]}" &
+
+
 echo "Rebuilding ftml files..."
 tools/absgenftml.py -q -t "AllChars (auto)"                      source/masters/Harmattan-Regular.ufo  tests/AllChars-auto.ftml        -l logs/AllChars.log        "${commonParams[@]}" -s "url(../results/Harmattan-Medium.ttf)|Med" -s "url(../results/Harmattan-SemiBold.ttf)|SeBld" -s "url(../results/Harmattan-Bold.ttf)|Bold" &
 tools/absgenftml.py -q -t "AL Sorted (auto)"                     source/masters/Harmattan-Regular.ufo  tests/ALsorted-auto.ftml        -l logs/ALsorted.log        "${commonParams[@]}" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
@@ -40,7 +43,7 @@ tools/absgenftml.py -q -t "DiacTest1 Short (auto)"               source/masters/
 tools/absgenftml.py -q -t "Subtending Marks (auto)"              source/masters/Harmattan-Regular.ufo  tests/SubtendingMarks-auto.ftml -l logs/Subtending.log      "${commonParams[@]}" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
 tools/absgenftml.py -q -t "DaggerAlef (auto)"                    source/masters/Harmattan-Regular.ufo  tests/DaggerAlef-auto.ftml      -l logs/DaggerAlef.log      "${commonParams[@]}" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
 tools/absgenftml.py -q -t "Yehbarree (auto)"                     source/masters/Harmattan-Regular.ufo  tests/Yehbarree-auto.ftml       -l logs/Yehbarree.log       "${commonParams[@]}" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
-tools/absgenftml.py -q -t "Feature-Language Interactions (auto)" source/masters/Harmattan-Regular.ufo  tests/FeatLang-auto.ftml        -l tests/logs/FeatLang.log  "${commonParams[@]}" -s "url(../results/Harmattan-Bold.ttf)|Bold"  &
+tools/absgenftml.py -q -t "Feature-Language Interactions (auto)" source/masters/Harmattan-Regular.ufo  tests/FeatLang-auto.ftml        -l tests/logs/FeatLang.log  "${commonParams[@]}"                                               &
 tools/absgenftml.py -q -t "Kerning (auto)"                       source/masters/Harmattan-Regular.ufo  tests/Kern-auto.ftml            -l logs/Kerning.log         "${commonParams[@]}" -s "url(../results/Harmattan-Medium.ttf)|Med" -s "url(../results/Harmattan-SemiBold.ttf)|SeBld" -s "url(../results/Harmattan-Bold.ttf)|Bold" &
 # tools/absgenftml.py -q -t "KernData with Marks (auto)"         source/masters/Harmattan-Regular.ufo  source/kerndata.ftml            -l source/logs/kerndata.log "${commonParams[@]}" -s "url(../results/Harmattan-Medium.ttf)|Med" -s "url(../results/Harmattan-SemiBold.ttf)|SeBld" -s "url(../results/Harmattan-Bold.ttf)|Bold" &
 wait
